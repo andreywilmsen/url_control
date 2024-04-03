38ae2e225450014e8692baa958909e85979dd5ea
Andrey Wilmsen de Paula <71944788+andreywilmsen@users.noreply.github.com>
2024-04-03 08:56:08 -0300

    Update conn.js

--- db/conn.js
+++ db/conn.js
@@ -1,6 +1,7 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb+srv://root:Andrey1309.@cluster0.nhew8ef.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGOD_CONNECT_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 let db = mongoose.connection;
 
